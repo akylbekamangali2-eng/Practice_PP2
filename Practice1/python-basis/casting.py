3be1398c9ca9c217e6d5eac4aeafc7601c8c2641
Amangali Akylbek <akylbekamangali2@gmail.com>
2026-01-25 18:56:17 +0500

    Added example for casting

--- Practice1/python-basis/casting.py
+++ Practice1/python-basis/casting.py
@@ -1,14 +1,17 @@
-x=int(123)
-y=int(23.4)
-z=int("3")
-print(x,y,z)
+#Specify a Variable Type
 
-a=float(123)
-b=float(123.4)
-c=float("12")
-print(a,b,c)
+#Integers
+x = int(3)   # x will be 3
+y = int(54.88) # y will be 54
+z = int("82") # z will be 82
 
-a1=str(123)
-b1=str(123.4)
-c1=str("12")
-print(a1,b1,c1)
+#Floats
+x = float(6)     # x will be 6.0
+y = float(43.26)   # y will be 43.26
+z = float("23")   # z will be 23.0
+w = float("14.66") # w will be 14.66
+
+#Strings
+x = str("number1") # x will be 'number1'
+y = str(468)    # y will be '468'
+z = str(44.0)  # z will be '44.0'
