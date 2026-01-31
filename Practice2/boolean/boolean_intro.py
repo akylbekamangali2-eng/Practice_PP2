5163b5ebc8af403f16d9ca60117d19e367696ba5
Amangali Akylbek <akylbekamangali2@gmail.com>
2026-01-31 10:36:14 +0500

    Add_practice2_boolean_intro_examples

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,14 @@
+#the basic of the boolean types
+a=True
+b=False
+print(a,b,a and b,a or b,sep=" ")
+#example to the boolean 
+name=input()
+age=int(input())
+gender=input()
+x=(name=="Akylbek")
+y=(age==18)
+z=(gender=="male")
+print(x+y+z)
+
+
